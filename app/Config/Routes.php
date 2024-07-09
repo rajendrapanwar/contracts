3691554603e67c82531f07200574a1dfab7eac52
Rajendra Panwar <rajendrapanwar8170@gmail.com>
2024-07-09 14:19:54 +0530

    changes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,3 +12,9 @@ $routes->get('customer_login', 'customer\Auth::login');
 $routes->get('customer_register', 'customer\Auth::register');
 $routes->post('customer_login', 'customer\Auth::login_post');
 $routes->post('customer_register', 'customer\Auth::register_post');
+
+//Contractor Routes
+$routes->get('contractor_login', 'contractor\Auth::login');
+$routes->get('contractor_register', 'contractor\Auth::register');
+$routes->post('contractor_login', 'contractor\Auth::login_post');
+$routes->post('contractor_register', 'contractor\Auth::register_post');
